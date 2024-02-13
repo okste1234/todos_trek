04976e80c3b6aa1c4fe62c5524f80c8d7ee7a6ac
okste1234 <okste1234@gmail.com>
2024-02-13 15:45:49 +0100

    chore: test that todo has completed; isDone has changed to true on toggle

--- test/Todos.ts
+++ test/Todos.ts
@@ -25,16 +25,11 @@ describe("ToDoTrek", function () {
       const { createTodo, todos, getAllTodos } = await loadFixture(deployTodosFixture);
 
       const tx = await createTodo("game", "I will play game by morning");
-      const list = await getAllTodos()
-      for (let index = 0; index < list.length; index++) {
-        const element = list[index];
-        
-        expect(element.title).to.equal("game");
-        expect(element.description).to.equal("I will play game by morning");
-
-      }
-      // const newTodoName = todos[todos.length -1].title;
-      // expect(newTodoName).to.equal("game");
+      const list = await getAllTodos();
+      const todoTitle = list[list.length - 1][0];
+      const todoDesc = list[list.length - 1][1];
+      expect(todoTitle).to.equal("game");
+      expect(todoDesc).to.equal("I will play game by morning");
     });
 
     it("Should pass on revertedWith if todos[index] to delete >= than todos.lenght", async function () {
@@ -55,96 +50,21 @@ describe("ToDoTrek", function () {
       await expect(toggleVal).to.be.revertedWith("Index out of bounds");
     });
 
-    //   it("Should set the right owner", async function () {
-    //     const { lock, owner } = await loadFixture(deployOneYearLockFixture);
+    it("Should test that todo has completed; isDone has changed to true on toggle", async function () {
+      const { toggleTodo, createTodo, getAllTodos, todos } = await loadFixture(deployTodosFixture);
 
-    //     expect(await lock.owner()).to.equal(owner.address);
-    //   });
 
-    //   it("Should receive and store the funds to lock", async function () {
-    //     const { lock, lockedAmount } = await loadFixture(
-    //       deployOneYearLockFixture
-    //     );
+      const setValue = await createTodo("eat", "I will eat rice later today")
 
-    //     expect(await ethers.provider.getBalance(lock.target)).to.equal(
-    //       lockedAmount
-    //     );
-    //   });
+      // console.log(todos as any[todos.length]);
 
-    //   it("Should fail if the unlockTime is not in the future", async function () {
-    //     // We don't use the fixture here because we want a different deployment
-    //     const latestTime = await time.latest();
-    //     const Lock = await ethers.getContractFactory("Lock");
-    //     await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-    //       "Unlock time should be in the future"
-    //     );
-    //   });
-    // });
-
-    // describe("Withdrawals", function () {
-    //   describe("Validations", function () {
-    //     it("Should revert with the right error if called too soon", async function () {
-    //       const { lock } = await loadFixture(deployOneYearLockFixture);
-
-    //       await expect(lock.withdraw()).to.be.revertedWith(
-    //         "You can't withdraw yet"
-    //       );
-    //     });
-
-    //     it("Should revert with the right error if called from another account", async function () {
-    //       const { lock, unlockTime, otherAccount } = await loadFixture(
-    //         deployOneYearLockFixture
-    //       );
-
-    //       // We can increase the time in Hardhat Network
-    //       await time.increaseTo(unlockTime);
-
-    //       // We use lock.connect() to send a transaction from another account
-    //       await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-    //         "You aren't the owner"
-    //       );
-    //     });
-
-    //     it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-    //       const { lock, unlockTime } = await loadFixture(
-    //         deployOneYearLockFixture
-    //       );
-
-    //       // Transactions are sent using the first signer by default
-    //       await time.increaseTo(unlockTime);
-
-    //       await expect(lock.withdraw()).not.to.be.reverted;
-    //     });
-    //   });
-
-    //   describe("Events", function () {
-    //     it("Should emit an event on withdrawals", async function () {
-    //       const { lock, unlockTime, lockedAmount } = await loadFixture(
-    //         deployOneYearLockFixture
-    //       );
-
-    //       await time.increaseTo(unlockTime);
-
-    //       await expect(lock.withdraw())
-    //         .to.emit(lock, "Withdrawal")
-    //         .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-    //     });
-    //   });
-
-    //   describe("Transfers", function () {
-    //     it("Should transfer the funds to the owner", async function () {
-    //       const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-    //         deployOneYearLockFixture
-    //       );
-
-    //       await time.increaseTo(unlockTime);
-
-    //       await expect(lock.withdraw()).to.changeEtherBalances(
-    //         [owner, lock],
-    //         [lockedAmount, -lockedAmount]
-    //       );
-    //     });
-    //   });
+      const toggle = await toggleTodo(todos.length)
+      
+      
+      const list = await getAllTodos()  
+      const val = list[list.length - 1][2]
+      expect(val).to.equal(true);
+    });
     });
   })
 
